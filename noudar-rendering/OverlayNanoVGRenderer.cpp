6e8e45231225eefc6a394d48260a3aa035ef38f3
= <danielmonteiro@id.uff.br>
2016-12-30 22:19:35 +0000

    Condition the NanoVG implementation to GL version
    Not working well for GL 1.1 yet (I believe it requires GL 2), but still
    works for GLES 2.

--- noudar-rendering/OverlayNanoVGRenderer.cpp
+++ noudar-rendering/OverlayNanoVGRenderer.cpp
@@ -6,21 +6,40 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
 
+#include "nanovg.h"
+
 #ifdef __APPLE__
 #if TARGET_IOS
+
+#define NANOVG_GLES2_IMPLEMENTATION
+
 #import <OpenGLES/ES2/gl.h>
 #import <OpenGLES/ES2/glext.h>
 #else
+
+#define NANOVG_GL2_IMPLEMENTATION
 #import <OpenGL/OpenGL.h>
 #import <OpenGL/gl3.h>
 #endif
-#else
+#endif
+
+#if defined(__ANDROID__ ) || defined(__EMSCRIPTEN__) || defined(MESA_GLES2)
+
+#define NANOVG_GLES2_IMPLEMENTATION
 
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 #include <EGL/egl.h>
+#else
+
+#define NANOVG_GL2_IMPLEMENTATION
+
+#include <GL/gl.h>
 
 #endif
+
+#include "nanovg_gl.h"
+#include "nanovg_gl_utils.h"
 
 #include <memory>
 #include <vector>
@@ -51,18 +70,20 @@
 #include "Scene.h"
 #include "Common.h"
 #include "VBORenderingJob.h"
-#include "DungeonGLES2Renderer.h"
-#include "NoudarDungeonSnapshot.h"
 
+#include "Vec2i.h"
+#include "NativeBitmap.h"
+#include "IMapElement.h"
+#include "CActor.h"
+#include "CGameDelegate.h"
+#include "CMap.h"
+#include "IRenderer.h"
+#include "CKnight.h"
+
+#include "NoudarDungeonSnapshot.h"
 
 #include "OverlayNanoVGRenderer.h"
 
-#include "nanovg.h"
-
-#define NANOVG_GLES2_IMPLEMENTATION
-
-#include "nanovg_gl.h"
-#include "nanovg_gl_utils.h"
 
 NVGcontext* mContext;
 
@@ -87,7 +108,17 @@ namespace odb {
     void OverlayNanoVGRenderer::render(const odb::NoudarDungeonSnapshot &snapshot) {
 
         if ( mFontData.size() > 0 ) {
+
+#ifdef NANOVG_GLES2_IMPLEMENTATION
             mContext = 	nvgCreateGLES2(NVG_ANTIALIAS | NVG_STENCIL_STROKES );
+#else
+            mContext = 	nvgCreateGL2(NVG_ANTIALIAS | NVG_STENCIL_STROKES );
+#endif
+
+            if ( mContext ) {
+                std::cout << "NVG context is faulty as salty" << std::endl;
+            }
+
             unsigned char* data = (unsigned char*)malloc(mFontData.size());
             std::copy(mFontData.begin(), mFontData.end(), data);
             nvgCreateFontMem( mContext, "font", data, mFontData.size(),  1 );
@@ -97,7 +128,6 @@ namespace odb {
         glViewport(0, 0, mWidth, mHeight);
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glEnable(GL_CULL_FACE);
         glDisable(GL_DEPTH_TEST);
 
         nvgBeginFrame(mContext, mWidth, mHeight, mWidth / mHeight);
@@ -105,25 +135,35 @@ namespace odb {
         nvgBeginPath(mContext);
         nvgFillColor(mContext, nvgRGBA(255, 255, 255, 128));
         nvgStrokeColor(mContext, nvgRGBA(255,0,0, 255));
-        nvgRoundedRect(mContext, 5, 5, 200, 100, 2.0f );
-        nvgCircle( mContext, 30, 30 , 50 );
-
-        nvgFill(mContext);
-        nvgStroke(mContext);
-
 
         nvgFontSize(mContext, 18.0f);
         nvgFontFace(mContext, "font");
         nvgFillColor(mContext, nvgRGBA(255,255,255,128));
-
         nvgTextAlign(mContext,NVG_ALIGN_LEFT|NVG_ALIGN_MIDDLE);
-        nvgText(mContext, 50, 370, "Monty rendering text in Dungeons of Noudar", nullptr);
 
+        for ( int y = 0; y < Knights::kMapSize; ++y ) {
+            for ( int x = 0; x < Knights::kMapSize; ++x ) {
+
+                if ( snapshot.ids[ y ][ x ] > 0 ) {
+                    nvgRoundedRect(mContext, 10 + (10 * x), 10 + ( 10 * y ), 10, 10, 2.0f );
+                }
+
+                std::string element;
+                element = snapshot.map[ y ][ x ];
+                nvgText(mContext, 10 + (10 * x), 10 + ( 10 * y ), element.c_str(), nullptr);
+                element = snapshot.snapshot[ y ][ x ];
+                nvgText(mContext, 10 + (10 * x), 10 + ( 10 * y ), element.c_str(), nullptr);
+            }
+        }
+
+
+
+        nvgFill(mContext);
+        nvgStroke(mContext);
 
         nvgEndFrame(mContext);
 
         glDisable(GL_BLEND);
-        glDisable(GL_CULL_FACE);
         glEnable(GL_DEPTH_TEST);
     }
 
