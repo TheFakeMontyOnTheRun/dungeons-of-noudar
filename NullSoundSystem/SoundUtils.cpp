f317e02ddb7981c944583ba2806acc579be3eb40
= <danielmonteiro@id.uff.br>
2017-01-26 08:40:32 +0000

    Rename AppleSoundSystem to NullSoundSystem
    Since I never got around to properly implement a sound system for apple
    machines (and since I found out how to use OpenAL on such systems),
    instead of simply deleting it, I've decided to rename it
    NullSoundSystem. It might come handy for testing purposes and for new
    ports, when I might want to tackle one issue at the time, but keep the
    code buildable.

---
+++ NullSoundSystem/SoundUtils.cpp
@@ -0,0 +1,13 @@
+//
+// Created by monty on 10/10/16.
+//
+#include <memory>
+
+#include "SoundClip.h"
+#include "SoundUtils.h"
+
+namespace odb {
+    std::shared_ptr<SoundClip> makeSoundClipFrom(FILE* source) {
+        return std::make_shared<SoundClip>(nullptr, 0, 0, 0, 0, 0 );
+    }
+}
