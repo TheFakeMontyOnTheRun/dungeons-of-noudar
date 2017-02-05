348a47d3d3eeea4f95f4a5a6f44ad5ccffe32e83
= <danielmonteiro@id.uff.br>
2017-02-05 16:55:48 +0000

    Unify the palette calculation for simpler maintenance

--- noudar-rendering/DungeonGL11Renderer.cpp
+++ noudar-rendering/DungeonGL11Renderer.cpp
@@ -271,18 +271,7 @@ namespace odb {
         glEnable( GL_TEXTURE_2D );
 
 	    glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST );
-
-	    if (kFogEnabled) {
-		    glFogi(GL_FOG_MODE, GL_LINEAR);        // Fog Mode
-		    float fogColor[4] = {0.5f, 0.5f, 0.5f};
-		    glFogfv(GL_FOG_COLOR, fogColor);            // Set Fog Color
-		    glFogf(GL_FOG_DENSITY, 0.35f);              // How Dense Will The Fog Be
-		    glHint(GL_FOG_HINT, GL_FASTEST);          // Fog Hint Value
-		    glFogf(GL_FOG_START, 1.0f);             // Fog Start Depth
-		    glFogf(GL_FOG_END, 5.0f);               // Fog End Depth
-		    glEnable(GL_FOG);                   // Enables GL_FOG
-	    }
-        gProgram = createProgram(vertexShader.c_str(), fragmentShader.c_str());
+	    gProgram = createProgram(vertexShader.c_str(), fragmentShader.c_str());
 
         if (!gProgram) {
             odb::Logger::log("Could not create program.");
