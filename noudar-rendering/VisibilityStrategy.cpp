b8742038214a754278526f52e2871bd7d4bf9f8e
= <danielmonteiro@id.uff.br>
2017-02-05 00:15:51 +0000

    Keep the visibility short for fog

--- noudar-rendering/VisibilityStrategy.cpp
+++ noudar-rendering/VisibilityStrategy.cpp
@@ -136,6 +136,10 @@ namespace odb {
 				continue;
 			}
 
+			if ( std::abs(offset.x) > 7 || std::abs(offset.y) > 7 ) {
+				continue;
+			}
+
 			if (visMap[currentPos.y][currentPos.x] == EVisibility::kVisible) {
 				continue;
 			}
