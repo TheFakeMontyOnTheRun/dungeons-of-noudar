541cf655dc30a1931611d701c80084c0b2aaa58e
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2017-04-10 21:36:01 +0100

    Help solving the link issues on Windows

--- DungeonsOfNoudarSDL/SDL-version/main.cpp
+++ DungeonsOfNoudarSDL/SDL-version/main.cpp
@@ -39,7 +39,7 @@
 #include "GameNativeAPI.h"
 #include "WindowOperations.h"
 
-int main(int argc, char *argv[]) {
+extern "C" int main(int argc, char **argv) {
 	readMap( std::make_shared<Knights::CPlainFileLoader>("res/"), "tiles.properties" );
 	initWindow();
 	setMainLoop();
