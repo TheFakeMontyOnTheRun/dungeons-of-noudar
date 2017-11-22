66d0612768790296ec4cbbe01d1047e4cbaad43d
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2017-11-22 18:41:06 +0000

    Double pixels in SDL version of 486 port
    This is meant to make it easier to see what's going on

--- DungeonsOfNoudar486/SDLVersion/CSDLRenderer.cpp
+++ DungeonsOfNoudar486/SDLVersion/CSDLRenderer.cpp
@@ -64,7 +64,7 @@ namespace odb {
 
     CRenderer::CRenderer() {
         SDL_Init( SDL_INIT_EVERYTHING );
-        video = SDL_SetVideoMode( 320, 200, 32, 0 );
+        video = SDL_SetVideoMode( 640, 400, 32, 0 );
 
         for ( int r = 0; r < 256; ++r ) {
             for ( int g = 0; g < 256; ++g ) {
@@ -167,10 +167,10 @@ namespace odb {
         for ( int y = 0; y  < 128; ++y ) {
             for ( int x = 0; x < 320; ++x ) {
                 SDL_Rect rect;
-                rect.x = x;
-                rect.y = y;
-                rect.w = 1;
-                rect.h = 1;
+                rect.x = 2 * x;
+                rect.y = 2 * y;
+                rect.w = 2;
+                rect.h = 2;
                 auto pixel = mPalette[ mBuffer[ (320 * y ) + x ] ];
 
                 SDL_FillRect(video, &rect, SDL_MapRGB(video->format, ((pixel & 0x000000FF)), ((pixel & 0x0000FF00) >> 8),
