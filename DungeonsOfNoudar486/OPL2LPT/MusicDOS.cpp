698fd15ac61d9de67dcd42162400a8bde1a5b63d
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2018-01-26 21:36:17 +0000

    Remove PC Speaker code
    It will be rewritten to use interrupt timer

--- DungeonsOfNoudar486/OPL2LPT/MusicDOS.cpp
+++ DungeonsOfNoudar486/OPL2LPT/MusicDOS.cpp
@@ -25,11 +25,7 @@ using eastl::array;
 
 bool enableOPL2 = false;
 extern OPL2 opl2;
-std::vector<int> noSound;
-std::vector<int> melody;
-std::vector<int>::const_iterator currentSoundPosition;
-int currentNote = 0;
-std::string buffer[3];
+char buffer[3][255];
 
 short get_lpt_port(int i) {
     return _farpeekw(_dos_ds, 0x0408 + (2 * (i - 1)));
@@ -77,19 +73,6 @@ short setup(void) {
     return 0;
 }
 
-
-void playSound(const std::vector<int> &sound) {
-
-    if ( sound.empty() ) {
-        melody.clear();
-        return;
-    }
-
-    melody = sound;
-    currentSoundPosition = std::begin(melody);
-}
-
-
 void playMusic(const std::string &music) {
     if (enableOPL2) {
         if (music.empty()) {
@@ -100,67 +83,21 @@ void playMusic(const std::string &music) {
         auto melody2 = music.substr( music.find(';') + 1 );
         auto melody3 = melody2.substr( melody2.find(';') + 1 );
         melody2 = melody2.substr(0, melody2.find(';'));
-        buffer[0] = melody1;
-        buffer[1] = melody2;
-        buffer[2] = melody3;
-        music_set(buffer[0].c_str(), buffer[1].c_str(), buffer[2].c_str());
+
+        snprintf(&buffer[0][0], 254, "%s", melody1.c_str());
+        snprintf(&buffer[1][0], 254, "%s", melody2.c_str());
+        snprintf(&buffer[2][0], 254, "%s", melody3.c_str());
+        music_set(&buffer[0][0], &buffer[1][0], &buffer[2][0]);
         return;
     }
-
-    int frequency = 0;
-    melody.clear();
-    for (const auto &note : music) {
-        switch (note) {
-            case 'a':
-            case 'A':
-                frequency = 932;
-                break;
-            case 'B':
-            case 'b':
-                frequency = 988;
-                break;
-            case 'C':
-            case 'c':
-                frequency = 1109;
-                break;
-            case 'D':
-            case 'd':
-                frequency = 1175;
-                break;
-            case 'E':
-            case 'e':
-                frequency = 1318;
-                break;
-            case 'F':
-            case 'f':
-                frequency = 1397;
-                break;
-            case 'G':
-            case 'g':
-                frequency = 1568;
-                break;
-            case ';':
-                melody.clear();
-                continue;
-        }
-        melody.push_back(frequency);
-    }
-    playSound(melody);
 }
 
 void muteSound() {
-    if ( !enableOPL2 ) {
-        nosound();
-        melody.clear();
-        currentNote = 0;
-    } else {
-        playMusic("");
-    }
+    playMusic("");
 }
 
 void playTune(const std::string &music) {
-    buffer[0] = music + ";" + music + ";" + music;
-    hackTune( music.c_str() );
+    playMusic( music + ";" + music + ";" + music );
 }
 
 void setupOPL2() {
@@ -170,38 +107,14 @@ void setupOPL2() {
     enableOPL2 = true;
 }
 
-void soundFrequency(int frequency) {
-    if (!enableOPL2) {
-        if (frequency != 0 && currentNote != frequency ) {
-            sound( frequency );
-            currentNote = frequency;
-        }
-    }
-}
-
-
 void soundTick() {
     if (enableOPL2) {
         music_loop();
-    } else {
-        if (melody.empty()) {
-            return;
-        }
-
-        if (currentSoundPosition != std::end(melody)) {
-            soundFrequency(*currentSoundPosition);
-            currentSoundPosition = std::next(currentSoundPosition);
-        } else {
-            muteSound();
-            melody.clear();
-        }
     }
 }
 
 void stopSounds() {
     if (enableOPL2) {
         music_shutdown();
-    } else {
-        muteSound();
     }
 }
