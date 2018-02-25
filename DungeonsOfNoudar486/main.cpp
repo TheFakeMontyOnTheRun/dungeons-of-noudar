7432f680b2d0e5c4281694a6a0712a2e59c4a6e7
Daniel Monteiro <danielmonteiro@id.uff.br>
2018-02-25 13:11:14 +0000

    Extract the main loop
    And as a bonus, fix the Emscripten version! Hoohay!

--- DungeonsOfNoudar486/main.cpp
+++ DungeonsOfNoudar486/main.cpp
@@ -58,6 +58,11 @@ using sg14::fixed_point;
 #include "CPackedFileReader.h"
 #include "LoadPNG.h"
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten/html5.h>
+#include <emscripten/emscripten.h>
+#endif
+
 enum class ESoundDriver {
     kNone, kPcSpeaker, kOpl2Lpt, kTandy, kCovox
 };
@@ -74,8 +79,10 @@ std::string stateText = "";
 std::string stateTransitionText = "";
 uint8_t bgColour;
 uint8_t textStartingLine;
+clock_t diff = 0;
+std::shared_ptr<odb::CPackedFileReader> fileLoader = nullptr;
 
-void setScreenState( EScreenState newState, std::shared_ptr<Knights::CGame> game, std::shared_ptr<odb::CPackedFileReader> fileLoader ) {
+void setScreenState( EScreenState newState, std::shared_ptr<Knights::CGame> game ) {
 
     switch( newState ) {
         case EScreenState::kIntro:
@@ -118,33 +125,41 @@ void setScreenState( EScreenState newState, std::shared_ptr<Knights::CGame> game
             textStartingLine = 2;
             break;
         case EScreenState::kExit:
+#ifdef __EMSCRIPTEN__
+            game->setIsPlaying(true);
+#else
             game->setIsPlaying(false);
+#endif
             break;
     }
 
     screenState = newState;
 }
 
-void onContinuePressed( std::shared_ptr<Knights::CGame> game, std::shared_ptr<odb::CPackedFileReader> fileLoader ) {
+void onContinuePressed( std::shared_ptr<Knights::CGame> game) {
     switch( screenState ) {
         case EScreenState::kIntro:
             game->playLevel(0);
             break;
         case EScreenState::kLoading:
-            setScreenState( EScreenState::kChapter, game, fileLoader );
+            setScreenState( EScreenState::kChapter, game);
             break;
         case EScreenState::kChapter:
-            setScreenState( EScreenState::kGame, game, fileLoader );
+            setScreenState( EScreenState::kGame, game);
             break;
         case EScreenState::kGame:
             break;
         case EScreenState::kVictory:
-            setScreenState( EScreenState::kExit, game, fileLoader );
+            setScreenState( EScreenState::kExit, game);
             break;
         case EScreenState::kGameOver:
-            setScreenState( EScreenState::kExit, game, fileLoader );
+            setScreenState( EScreenState::kExit, game);
             break;
         case EScreenState::kExit:
+#ifdef __EMSCRIPTEN__
+            game->setIsPlaying(true);
+            setScreenState( EScreenState::kIntro, game);
+#endif
             break;
     }
 }
@@ -211,9 +226,8 @@ void renderTick(long ms) {
 
 std::shared_ptr<Knights::CGame> game;
 
-void playerIsDead(std::shared_ptr<odb::CPackedFileReader> fileLoader) {
-    setScreenState(EScreenState::kGameOver, game, fileLoader);
-    renderTick(50);
+void playerIsDead() {
+    setScreenState(EScreenState::kGameOver, game);
 }
 
 void handleConsoleLines(Knights::CommandType command, int playerHealthDiff, int targetHealthDiff,
@@ -278,6 +292,63 @@ void handleConsoleLines(Knights::CommandType command, int playerHealthDiff, int 
         renderer->appendToLog(buffer, playerGainedFaithColour);
         renderer->startHealHighlight();
         playTune("t200i52o4defg");
+    }
+}
+
+void loopTick() {
+
+    int healthAtTargetBefore = 0;
+    int healthAtTargetAfter = 0;
+    clock_t t0 = 0;
+    clock_t t1 = 0;
+
+    t0 = uclock();
+
+    auto playerHealthBefore = game->getMap()->getAvatar()->getHP();
+    auto cursorPosition = game->getMap()->getTargetProjection(game->getMap()->getAvatar());
+    auto actorAtTarget = game->getMap()->getActorAt(cursorPosition);
+
+    if (actorAtTarget != nullptr) {
+        healthAtTargetBefore = actorAtTarget->getHP();
+    } else {
+        healthAtTargetBefore = 0;
+    }
+
+    game->tick();
+#ifdef __EMSCRIPTEN__
+    diff = 50;
+#endif
+    renderTick(diff);
+    Knights::CommandType command = odb::renderer->peekInput();
+    game->tick();
+    soundTick();
+
+    if (actorAtTarget != nullptr) {
+        healthAtTargetAfter = actorAtTarget->getHP();
+    } else {
+        healthAtTargetAfter = 0;
+    }
+
+    auto targetHealthDiff = healthAtTargetAfter - healthAtTargetBefore;
+    auto playerHealthAfter = game->getMap()->getAvatar()->getHP();
+    auto playerHealthDiff = playerHealthAfter - playerHealthBefore;
+
+    handleConsoleLines(command, playerHealthDiff, targetHealthDiff, odb::renderer, actorAtTarget);
+
+    t1 = uclock();
+    diff = (1000 * (t1 - t0)) / UCLOCKS_PER_SEC;
+    if (diff == 0) {
+        diff = 1;
+    }
+
+    if (!game->getMap()->getAvatar()->isAlive()) {
+        playerIsDead();
+    }
+
+    if ( command == 13 ) {
+
+        odb::renderer->mBufferedCommand = '.';
+        onContinuePressed(game);
     }
 }
 
@@ -349,28 +420,29 @@ int main(int argc, char **argv) {
     const auto LEVEL_LIMIT = 7;
 
     auto delegate = std::make_shared<Knights::CGameDelegate>();
-    auto fileLoader = std::make_shared<odb::CPackedFileReader>("data.pfs");
+    fileLoader = std::make_shared<odb::CPackedFileReader>("data.pfs");
     odb::renderer = std::make_shared<odb::CRenderer>(fileLoader);
     game = std::make_shared<Knights::CGame>(fileLoader, odb::renderer, delegate);
 
 
-    setScreenState(EScreenState::kIntro, game, fileLoader);
+    setScreenState(EScreenState::kIntro, game);
 
     auto onLevelWillLoad = [&]() {
 
         if (game != nullptr) {
             if (game->getLevelNumber() >= LEVEL_LIMIT) {
-                game->setIsPlaying(false);
 
-                setScreenState(EScreenState::kVictory, game, fileLoader);
-                renderTick(50);
+#ifndef __EMSCRIPTEN__
+                game->setIsPlaying(false);
+#endif
+
+                setScreenState(EScreenState::kVictory, game);
                 return;
             }
         }
 
-        setScreenState(EScreenState::kLoading, game, fileLoader);
+        setScreenState(EScreenState::kLoading, game);
         renderTick(50);
-
     };
 
     delegate->setOnLevelWillLoadCallback(onLevelWillLoad);
@@ -383,65 +455,17 @@ int main(int argc, char **argv) {
                                         tileProperties);
         }
 
-        setScreenState(EScreenState::kChapter, game, fileLoader);
-        renderTick(50);
+        setScreenState(EScreenState::kChapter, game);
     };
     delegate->setOnLevelLoadedCallback(onLevelLoaded);
 
-    int healthAtTargetBefore = 0;
-    int healthAtTargetAfter = 0;
-    clock_t diff = 0;
-    clock_t t0;
-    clock_t t1;
-
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(loopTick, 20, 1);
+#else
     while (game->isPlaying()) {
-        t0 = uclock();
-
-        auto playerHealthBefore = game->getMap()->getAvatar()->getHP();
-        auto cursorPosition = game->getMap()->getTargetProjection(game->getMap()->getAvatar());
-        auto actorAtTarget = game->getMap()->getActorAt(cursorPosition);
-
-        if (actorAtTarget != nullptr) {
-            healthAtTargetBefore = actorAtTarget->getHP();
-        } else {
-            healthAtTargetBefore = 0;
-        }
-
-        game->tick();
-        renderTick(diff);
-        Knights::CommandType command = odb::renderer->peekInput();
-        game->tick();
-        soundTick();
-
-        if (actorAtTarget != nullptr) {
-            healthAtTargetAfter = actorAtTarget->getHP();
-        } else {
-            healthAtTargetAfter = 0;
-        }
-
-        auto targetHealthDiff = healthAtTargetAfter - healthAtTargetBefore;
-        auto playerHealthAfter = game->getMap()->getAvatar()->getHP();
-        auto playerHealthDiff = playerHealthAfter - playerHealthBefore;
-
-        handleConsoleLines(command, playerHealthDiff, targetHealthDiff, odb::renderer, actorAtTarget);
-
-        t1 = uclock();
-        diff = (1000 * (t1 - t0)) / UCLOCKS_PER_SEC;
-        if (diff == 0) {
-            diff = 1;
-        }
-
-        if (!game->getMap()->getAvatar()->isAlive()) {
-            playerIsDead(fileLoader);
-        }
-
-        if ( command == 13 ) {
-
-            odb::renderer->mBufferedCommand = '.';
-            onContinuePressed(game, fileLoader);
-            renderTick(50);
-        }
+        loopTick();
     }
+#endif
 
     if (soundDriver != ESoundDriver::kNone) {
         stopSounds();
