0c0d2284f08a8eed400ed4868416bf17b043eafe
Daniel Monteiro <danielmonteiro@id.uff.br>
2019-07-22 20:49:13 +0100

    Update shaders on other ports as well

--- DungeonsOfNoudar-NDK/app/src/main/assets/fragment.glsl
+++ DungeonsOfNoudar-NDK/app/src/main/assets/fragment.glsl
@@ -1,21 +1,39 @@
+#ifdef GL_ES
 precision mediump float;
+#endif
+
+
+#if __VERSION__ >= 140
+in vec2 vTextureCoords;
+in vec4 vColour;
+in float distance;
+out vec4 fragColor;
+#else
 varying vec2 vTextureCoords;
 varying vec4 vColour;
 varying float distance;
+#endif
+
 uniform sampler2D sTexture;
 uniform vec4 uMod;
 uniform vec4 uFade;
 uniform vec4 uFog;
 
 void main() {
+    #if __VERSION__ >= 140
+    fragColor = texture2D( sTexture, vTextureCoords );
 
-   
+    if ( fragColor.a < 0.5 ) {
+        discard;
+    }
+    fragColor = fragColor * uFade.a * uMod;
+
+    #else
     gl_FragColor = texture2D( sTexture, vTextureCoords );
 
     if ( gl_FragColor.a < 0.5 ) {
         discard;
     }
-
-  gl_FragColor = gl_FragColor * uFade.a * uMod;
-  
+    gl_FragColor = gl_FragColor * uFade.a * uMod;
+    #endif
 }
--- DungeonsOfNoudarX11/res/vertex.glsl
+++ DungeonsOfNoudarX11/res/vertex.glsl
@@ -1,13 +1,28 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+
+#if __VERSION__ >= 140
+in vec4 aPosition;
+in vec4 aColour;
+in vec2 aTexCoord;
+out float distance;
+out vec2 vTextureCoords;
+out vec4 vColour;
+#else
 attribute vec4 aPosition;
 attribute vec4 aColour;
 attribute vec2 aTexCoord;
-uniform mat4 uModel;
-uniform mat4 uScale;
-uniform mat4 uProjection;
-uniform mat4 uView;
 varying float distance;
 varying vec2 vTextureCoords;
 varying vec4 vColour;
+#endif
+
+uniform mat4 uView;
+uniform mat4 uModel;
+uniform mat4 uScale;
+uniform mat4 uProjection;
 
 void main() {
     gl_Position =  uProjection * uView * uModel * aPosition;
