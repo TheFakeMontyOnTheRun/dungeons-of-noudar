fb9270ab41d8568d176f0fcee3051a2a9bf7f872
Daniel Monteiro <danielmonteiro@id.uff.br>
2020-01-28 10:34:43 +0000

    Update sound, key detection and configuration dynamically

--- DungeonsOfNoudarNDK/app/src/main/java/pt/b13h/noudar/MainActivity.kt
+++ DungeonsOfNoudarNDK/app/src/main/java/pt/b13h/noudar/MainActivity.kt
@@ -1,20 +1,23 @@
 package pt.b13h.noudar
 
-import android.content.res.AssetManager
+import android.content.Context
+import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.SoundPool
 import android.os.Build
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.nio.ByteBuffer
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var soundPool : SoundPool
+    private var soundPool : SoundPool? = null
     private var sounds = IntArray(8)
     private var pixels = ByteArray(320 * 240 * 4)
     val bitmap: Bitmap = Bitmap.createBitmap(320, 240, Bitmap.Config.ARGB_8888)
@@ -31,69 +34,133 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             SoundPool(5, AudioManager.STREAM_MUSIC, 0 )
         }
 
-        sounds[0] = soundPool.load(this, R.raw.menu_move, 1)
-        sounds[1] = soundPool.load(this, R.raw.menu_select, 1)
-        sounds[2] = soundPool.load(this, R.raw.gotclue, 1)
-        sounds[3] = soundPool.load(this, R.raw.detected, 1)
-        sounds[4] = soundPool.load(this, R.raw.detected2, 1)
-        sounds[5] = soundPool.load(this, R.raw.menu_select, 1)
-        sounds[6] = soundPool.load(this, R.raw.menu_select, 1)
-        sounds[7] = soundPool.load(this, R.raw.hurt, 1)
 
-
+        sounds[0] = soundPool!!.load(this, R.raw.menu_move, 1)
+        sounds[1] = soundPool!!.load(this, R.raw.menu_select, 1)
+        sounds[2] = soundPool!!.load(this, R.raw.gotclue, 1)
+        sounds[3] = soundPool!!.load(this, R.raw.detected, 1)
+        sounds[4] = soundPool!!.load(this, R.raw.detected2, 1)
+        sounds[5] = soundPool!!.load(this, R.raw.menu_select, 1)
+        sounds[6] = soundPool!!.load(this, R.raw.menu_select, 1)
+        sounds[7] = soundPool!!.load(this, R.raw.hurt, 1)
     }
+
+
+    private fun enterStickyImmersiveMode() {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= 29) {
+            enterStickyImmersiveMode()
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
 
+        setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null ) {
             NoudarJNI.initAssets(resources.assets)
         }
 
+        if  ( (application as NoudarApplication).mayEnableSound() ) {
+            initAudio()
+        } else {
+            soundPool = null
+        }
 
-        btnUp.setOnClickListener(this)
-        btnDown.setOnClickListener(this)
-        btnFire.setOnClickListener(this)
-        btnPick.setOnClickListener(this)
-        btnAim.setOnClickListener(this)
-        btnLeft.setOnClickListener(this)
-        btnRight.setOnClickListener(this)
-        btnStart.setOnClickListener(this)
-
-        initAudio()
         imageView.setImageBitmap(bitmap)
     }
 
     override fun onDestroy() {
 
-        soundPool.release()
+        soundPool?.release()
         super.onDestroy()
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+
+        var toSend = '.'
+        when (keyCode) {
+            KeyEvent.KEYCODE_DPAD_UP, KeyEvent.KEYCODE_W -> toSend = 'w'
+            KeyEvent.KEYCODE_DPAD_DOWN, KeyEvent.KEYCODE_S -> toSend = 's'
+            KeyEvent.KEYCODE_DPAD_LEFT, KeyEvent.KEYCODE_Q -> toSend = 'q'
+            KeyEvent.KEYCODE_DPAD_RIGHT, KeyEvent.KEYCODE_E -> toSend = 'e'
+
+            KeyEvent.KEYCODE_BUTTON_L1, KeyEvent.KEYCODE_A -> toSend = 'a'
+            KeyEvent.KEYCODE_BUTTON_R1, KeyEvent.KEYCODE_D -> toSend = 'd'
+
+            KeyEvent.KEYCODE_BUTTON_A, KeyEvent.KEYCODE_Z -> toSend = 'z'
+            KeyEvent.KEYCODE_BUTTON_B, KeyEvent.KEYCODE_X-> toSend = 'x'
+            KeyEvent.KEYCODE_BUTTON_C, KeyEvent.KEYCODE_BUTTON_Y, KeyEvent.KEYCODE_C-> toSend = 'c'
+            KeyEvent.KEYCODE_BUTTON_START, KeyEvent.KEYCODE_BUTTON_X, KeyEvent.KEYCODE_ENTER -> toSend = '\n'
+            else -> return super.onKeyUp(keyCode, event )
+        }
+        NoudarJNI.sendCommand(toSend)
+        return true
     }
 
 
     override fun onResume() {
         super.onResume()
         running = true
+
+        Thread( Runnable {
+            while (running) {
+                runOnUiThread {
+                    if  ( !(application as NoudarApplication).hasPhysicalController() ) {
+
+                        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                            llActions.visibility = View.VISIBLE
+                            llDirections.visibility = View.VISIBLE
+                        } else {
+                            llScreenControllers.visibility = View.VISIBLE
+                        }
+
+                        btnUp.setOnClickListener(this)
+                        btnDown.setOnClickListener(this)
+                        btnFire.setOnClickListener(this)
+                        btnPick.setOnClickListener(this)
+                        btnAim.setOnClickListener(this)
+                        btnLeft.setOnClickListener(this)
+                        btnRight.setOnClickListener(this)
+                        btnStart.setOnClickListener(this)
+                    } else {
+                        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                            llActions.visibility = View.GONE
+                            llDirections.visibility = View.GONE
+                        } else {
+                            llScreenControllers.visibility = View.GONE
+                        }
+                    }
+                }
+
+                Thread.sleep(1000);
+            }
+        }).start()
+
         Thread(Runnable {
             while (running) {
                 runOnUiThread { redraw() }
-                Thread.sleep(50)
+                Thread.sleep(75)
             }
         }
         ).start()
 
-        Thread(Runnable {
-            while (running) {
-                when (val sound = NoudarJNI.getSoundToPlay()) {
-                    0, 1, 2, 3, 4, 5, 6, 7, 8 -> soundPool.play(sounds[sound], 1f, 1f, 0, 0, 1f)
+        if (soundPool != null ) {
+            Thread(Runnable {
+                while (running) {
+                    when (val sound = NoudarJNI.getSoundToPlay()) {
+                        0, 1, 2, 3, 4, 5, 6, 7, 8 -> soundPool!!.play(sounds[sound], 1f, 1f, 0, 0, 1f)
+                    }
+                    Thread.sleep(10)
                 }
-                Thread.sleep(10)
             }
+            ).start()
         }
-        ).start()
-
     }
 
     override fun onPause() {
---
+++ DungeonsOfNoudarNDK/app/src/main/java/pt/b13h/noudar/NoudarApplication.kt
@@ -0,0 +1,50 @@
+package pt.b13h.noudar
+
+import android.app.Application
+import android.content.Context
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.media.AudioManager
+import android.view.InputDevice
+import java.util.*
+
+class NoudarApplication : Application() {
+
+    fun mayEnableSound(): Boolean {
+        val am = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        return when (am.ringerMode) {
+            AudioManager.RINGER_MODE_NORMAL -> true
+            AudioManager.RINGER_MODE_SILENT, AudioManager.RINGER_MODE_VIBRATE -> false
+            else -> false
+        }
+    }
+
+    private fun hasTouchscreen(): Boolean {
+        return packageManager.hasSystemFeature(PackageManager.FEATURE_TOUCHSCREEN)
+    }
+
+    private fun hasPhysicalKeyboard(): Boolean {
+        return resources.configuration.keyboard != Configuration.KEYBOARD_NOKEYS
+    }
+
+
+    private fun hasGamepad(): Boolean {
+        val gameControllerDeviceIds: MutableList<Int> =
+            ArrayList()
+        val deviceIds = InputDevice.getDeviceIds()
+        for (deviceId in deviceIds) {
+            val dev = InputDevice.getDevice(deviceId)
+            val sources = dev.sources
+            if ((sources and InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD ||
+                ((sources and InputDevice.SOURCE_JOYSTICK) == InputDevice.SOURCE_JOYSTICK)
+            ) {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun hasPhysicalController() : Boolean {
+        return hasPhysicalKeyboard() || hasGamepad() || !hasTouchscreen()
+    }
+}
